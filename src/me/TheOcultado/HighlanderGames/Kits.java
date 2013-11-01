53b7320c1fe432f3c86cfb48d79610b1bcee8a37
Theminione <thomas@leahfamily.plus.com>
2013-11-01 17:16:00 +0000

    Formatted :P

--- src/me/TheOcultado/HighlanderGames/Kits.java
+++ src/me/TheOcultado/HighlanderGames/Kits.java
@@ -26,53 +26,53 @@ public class Kits {
 		Inventory inv = p.getInventory();
 		clearInventory(p);
 		if (kit.equalsIgnoreCase("Archer")) {
-			
+
 			ItemStack bow = new ItemStack(Material.BOW);
 			bow.addEnchantment(Enchantment.ARROW_DAMAGE, 2);
 			bow.addUnsafeEnchantment(Enchantment.KNOCKBACK, 2);
-			
+
 			ItemStack LeatherHelmet = new ItemStack(Material.LEATHER_HELMET);
 			LeatherHelmet.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
 			p.getInventory().setHelmet(LeatherHelmet);
-			
+
 			ItemStack LeatherChest = new ItemStack(Material.LEATHER_CHESTPLATE);
 			LeatherChest.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
 			p.getInventory().setChestplate(LeatherChest);
-			
+
 			ItemStack LeatherLegs = new ItemStack(Material.LEATHER_LEGGINGS);
 			LeatherLegs.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
 			p.getInventory().setLeggings(LeatherLegs);
-			
+
 			ItemStack LeatherBoots = new ItemStack(Material.LEATHER_BOOTS);
 			LeatherBoots.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 3);
 			p.getInventory().setBoots(LeatherBoots);
-			
+
 			ItemStack Arrows = new ItemStack(Material.ARROW, 16);
 			inv.addItem(bow);
 			inv.addItem(Arrows);
-			
-			}else if(kit .equalsIgnoreCase("Bomber")){
-				
+
+		} else if (kit.equalsIgnoreCase("Bomber")) {
+
 			ItemStack tnt = new ItemStack(Material.TNT, 16);
-			
+
 			ItemStack LeatherHelmet = new ItemStack(Material.LEATHER_HELMET);
 			LeatherHelmet.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 3);
 			p.getInventory().setHelmet(LeatherHelmet);
-			
+
 			ItemStack LeatherChest = new ItemStack(Material.LEATHER_CHESTPLATE);
 			LeatherChest.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 3);
 			p.getInventory().setChestplate(LeatherChest);
-			
+
 			ItemStack LeatherLegs = new ItemStack(Material.LEATHER_LEGGINGS);
 			LeatherLegs.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 3);
 			p.getInventory().setLeggings(LeatherLegs);
-			
+
 			ItemStack LeatherBoots = new ItemStack(Material.LEATHER_BOOTS);
 			LeatherBoots.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 3);
 			p.getInventory().setBoots(LeatherBoots);
-			
+
 			inv.addItem(tnt);
-				
+
 		}
 	}
 }
