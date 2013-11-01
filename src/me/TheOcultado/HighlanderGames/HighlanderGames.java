160d5acd8f2b7d28233d9e3e3ce975d26715f7ba
Theminione <thomas@leahfamily.plus.com>
2013-11-01 14:09:18 +0000

    Updated the Main class

--- src/me/TheOcultado/HighlanderGames/HighlanderGames.java
+++ src/me/TheOcultado/HighlanderGames/HighlanderGames.java
@@ -1,5 +1,25 @@
 package me.TheOcultado.HighlanderGames;
 
-public class HighlanderGames {
+import java.util.logging.Logger;
 
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HighlanderGames extends JavaPlugin {
+
+	public final Logger log = Logger.getLogger("Minecraft");
+	public static HighlanderGames plugin;
+
+	@Override
+	public void onEnable() {
+		PluginDescriptionFile pdfFile = getDescription();
+		this.log.info(pdfFile.getName() + " has been Enabled!");
+	}
+	
+	@Override
+	public void onDisable() {
+		PluginDescriptionFile pdfFile = getDescription();
+		this.log.info(pdfFile.getName() + " has been Disabled!");
+	}
+	
 }
